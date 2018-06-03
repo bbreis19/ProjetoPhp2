2719465911bf7302224a760d6975c31ae4d2e9da
Barbara Reis <tallytareis@gmail.com>
2018-06-03 23:16:38 +0000

    Colocando os dados do adm dentro da tabela no banco CERTO

--- ci/application/models/Insertdao.php
+++ ci/application/models/Insertdao.php
@@ -1,6 +1,11 @@
 <?php
- 
+    require_once APPPATH."models/formulario.php";
   
+  class InsertDAO extends CI_Model{
+      public function insert(Formulario $form){
+          $this->db->insert($form->getClassName(),$form->toArray());
+      }
+  }
   
  
  
