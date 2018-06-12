fa9b0feda3317a3b36bef64189e318a64ab57777
Barbara Reis <tallytareis@gmail.com>
2018-06-12 04:41:49 +0000

    Alteracao contato

--- ci/application/views/home.php
+++ ci/application/views/home.php
@@ -47,7 +47,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Contato</h2>
+            <h2 class="section-heading text-uppercase">Login</h2>
 
           </div>
         </div>
